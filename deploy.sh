e81184099704a493e156af6b196eecd02955b321
JiangShuuu <globelex65@gmail.com>
2022-02-16 10:02:53 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.Github.io/<REPO>
 # git push -f https://github.com/<USERNAME>/<REPO>.git main:gh-pages
-git push -f https://github.com/JiangShuuu/forum-front-end-vue.git main:gh-pages
+git push -f https://github.com/JiangShuuu/AlphaCamp-forum-front-end-vue.git main:gh-pages
 
 cd -
